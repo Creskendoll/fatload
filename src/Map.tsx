593ac09957456ceb7abfe3315822dda2396f7c11
Kenan Soylu <ken.soylu@hotmail.com>
2022-09-24 14:47:27 +0200

    goodle map

---
+++ src/Map.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+
+function Map() {
+    const ref = React.useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        new window.google.maps.Map(ref?.current as any, {
+            center: {
+                lat: 52.5,
+                lng: 13.4,
+            },
+            zoom: 12,
+        });
+    });
+    return (
+        <div style={{ height: "100vh", width: "100vw" }} ref={ref} id="map" />
+    );
+}
+
+export default Map;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Status, Wrapper } from "@googlemaps/react-wrapper";
+
+const render = (status: Status) => {
+    return <h1>{status}</h1>;
+};
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Wrapper
+            apiKey={"AIzaSyDNAJ4hwTlg3z58M3WLFuiLLCQ3h7DbcZI"}
+            render={render}
+        >
+            <App />
+        </Wrapper>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
