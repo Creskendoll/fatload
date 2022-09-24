e82d0310814c9fd9cfb4a07c5d1baa9364de3766
Ryan <ryanhopk@mtu.edu>
2022-09-24 20:40:43 +0200

    Add auto suggestions for start location

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,27 @@ import React from "react";
 import "./styles/App.css";
 import Map from "./Map";
 import Marker from "./Marker";
+import _ from "lodash";
 
 function App() {
+    const [startLocationValue, setStartLocationValue] = React.useState('');
+    const [startLocationSuggestions, setStartLocationSuggestions] = React.useState<google.maps.places.PlaceResult[] | null>(null);
     const [clicks, setClicks] = React.useState<google.maps.LatLng[]>([]);
     const [zoom, setZoom] = React.useState(12);
     const [center, setCenter] = React.useState<google.maps.LatLngLiteral>({
         lat: 52.5,
         lng: 13.4,
     });
+    const dbouncedStartSearch = React.useRef(_.debounce(search, 250));
     const dirService = React.useRef(new google.maps.DirectionsService());
     const dirRenderer = React.useRef(
         new google.maps.DirectionsRenderer({
             suppressMarkers: true,
             suppressBicyclingLayer: true,
         })
+    );
+    const placesService = React.useRef<google.maps.places.PlacesService | null>(
+        null
     );
 
     function fetchDestination(
@@ -52,10 +59,42 @@ function App() {
         setCenter(m.getCenter().toJSON());
 
         if (!dirRenderer.current.getMap()) dirRenderer.current.setMap(m);
+        if (!placesService.current) placesService.current = new google.maps.places.PlacesService(m);
     };
+
+    function search(query: String) {
+        let request = {
+            query: `${query}`
+        };
+
+        if (request.query) {
+            placesService.current?.textSearch(request, (result) => {
+                setStartLocationSuggestions(result);
+            });
+        }
+    }
+
+    React.useEffect(() => {
+        dbouncedStartSearch.current.cancel();
+        dbouncedStartSearch.current(startLocationValue);
+    }, [startLocationValue]);
 
     return (
         <div>
+            <div>
+                <input 
+                    type='text' 
+                    list='locations' 
+                    placeholder='Delivery start location' 
+                    value={startLocationValue}
+                    onChange={(e) => setStartLocationValue(e.target.value)}></input>
+                <datalist id='locations'>
+                    {startLocationSuggestions?.map((location, i) => {
+                        return <option key={i} value={location?.formatted_address}></option>
+                    })}
+                </datalist>
+            </div>
+
             <Map
                 style={{}}
                 center={center}
