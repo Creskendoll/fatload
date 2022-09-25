85827b753ff33cae446b758b3c5f0de3341dad7a
Alexander <alexander@Alexanders-MacBook-Air.local>
2022-09-25 12:29:20 +0200

    started combining 2 compnents

--- src/components/SearchDrawer.tsx
+++ src/components/SearchDrawer.tsx
@@ -10,6 +10,7 @@ interface Props {
 
 export default function SearchDrawer({ placesService }: Props) {
     const [startLocationValue, setStartLocationValue] = React.useState("");
+    const [currentLocation, setCurrentLocation] = React.useState("")
     const [startLocationSuggestions, setStartLocationSuggestions] =
         React.useState<google.maps.places.PlaceResult[] | null>(null);
     const dbouncedStartSearch = React.useRef(_.debounce(search, 250));
@@ -35,6 +36,16 @@ export default function SearchDrawer({ placesService }: Props) {
     return (
         <>
             <div className="searchWrapper">
+                { screenState === "search-location" && (
+                    <button
+                    className="current-location"
+                    onClick={() => setCurrentLocation("Lohmühlenstraße 65, 12435 Berlin")}
+                    >
+                        <span id="location-logo"></span>
+                        <span id="location-text">current location</span>
+                    </button>
+                )} 
+              
                 <input
                     onFocus={() => {
                         setScreenState("search-location");
@@ -43,7 +54,7 @@ export default function SearchDrawer({ placesService }: Props) {
                         setScreenState("map");
                     }}
                     type="text"
-                    className="littleSearchWrapper"
+                    className={screenState !== "search-location" ? "littleSearchWrapper" : "extendedSearchWrapper"}
                     placeholder="Where to?"
                     value={startLocationValue}
                     onChange={(e) => {
@@ -52,7 +63,7 @@ export default function SearchDrawer({ placesService }: Props) {
                     }}
                 >
                 </input>
-                <img className="searchIcon" src="searchIcon.png" alt="icon"/>
+                <img className={screenState !== "search-location" ? "searchIcon" : "searchIconExtended"} src="searchIcon.png" alt="icon"/>
                 <div>
                 {startLocationSuggestions?.map((location, i) => {
                         return (
@@ -79,3 +90,88 @@ export default function SearchDrawer({ placesService }: Props) {
         </>
     );
 }
+
+
+// const [startLocationValue, setStartLocationValue] = React.useState("");
+// const [currentLocation, setCurrentLocation] = React.useState("")
+// const [startLocationSuggestions, setStartLocationSuggestions] = React.useState<google.maps.places.PlaceResult[] | null>(null);
+// const dbouncedStartSearch = React.useRef(_.debounce(search, 250));
+// const [, setScreen] = React.useContext(ScreenContext);
+
+// React.useEffect(() => {
+//     dbouncedStartSearch.current.cancel();
+//     dbouncedStartSearch.current(startLocationValue);
+// }, [startLocationValue]);
+
+// function search(query: String) {
+//     let request = {
+//         query: `${query}`,
+//     };
+
+//     if (request.query) {
+//         placesService.current?.textSearch(request, (result) => {
+//             setStartLocationSuggestions(result);
+//         });
+//     }
+// }
+
+// return (
+//     <>
+//         <div>
+//             <button
+//                 className="current-location"
+//                 onClick={() => setCurrentLocation("Lohmühlenstraße 65, 12435 Berlin")}
+//             >
+//                 <span id="location-logo"></span>
+//                 <span id="location-text">current location</span>
+//             </button>
+//             <input
+//                 onFocus={() => {
+//                     setScreen("search-location");
+//                 }}
+//                 onBlur={() => {
+//                     setScreen("map");
+//                 }}
+//                 type="text"
+//                 className="form-control form-control-starting"
+//                 placeholder="Starting From?"
+//                 value={currentLocation}
+//                 onChange={(e) => {
+//                     setCurrentLocation(e.target.value)
+//                 }}
+//             ></input>
+//             <input
+//                 onFocus={() => {
+//                     setScreen("search-location");
+//                 }}
+//                 onBlur={() => {
+//                     setScreen("map");
+//                 }}
+//                 type="text"
+//                 className="form-control form-control-destination"
+//                 placeholder="Where to?"
+//                 value={startLocationValue}
+//                 onChange={(e) => {
+//                     setStartLocationValue(e.target.value)
+//                 }}
+//             ></input>
+//             <div>
+//                 {startLocationSuggestions?.map((location, i) => {
+//                     return (
+//                         i < 5 && (
+//                             <span
+//                                 onClick={(e) => {
+//                                     setStartLocationValue(location?.formatted_address || "")
+//                                 }}
+//                                 key={i}
+//                             >
+//                                 {location?.formatted_address}
+//                             </span>
+//                         )
+//                     );
+//                 })}
+//             </div>
+//         </div>
+//         <button onClick={() => setScreen("in-progress")}>Order</button>
+//     </>
+// );
